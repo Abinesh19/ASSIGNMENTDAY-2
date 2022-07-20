933b8d32d2a34ea597e22fb6ad75f127bf137657
Abinesh19 <107668169+Abinesh19@users.noreply.github.com>
2022-07-20 22:49:47 +0530

    Add files via upload
    JUL07.DAY20

---
+++ jul07/MyMobile.java
@@ -0,0 +1,25 @@
+package day20.jul07;
+
+public class MyMobile {
+private void sendMessage() {
+	// TODO Auto-generated method stub
+	System.out.println("send message");
+
+}
+private void shareDocument() {
+	// TODO Auto-generated method stub
+	System.out.println("share document");
+
+}
+private void call() {
+	// TODO Auto-generated method stub
+	System.out.println("call");
+
+}
+public static void main(String[] args) {
+	MyMobile mob=new MyMobile();
+	mob.sendMessage();
+	mob.shareDocument();
+	mob.call();
+}
+}
