4c096805b4110e1ec3061567ce5b85dfacd2ab33
Abinesh19 <107668169+Abinesh19@users.noreply.github.com>
2022-07-20 22:43:42 +0530

    Add files via upload
    JUL07.DAY20

---
+++ jul07/MyCar.java
@@ -0,0 +1,31 @@
+package day20.jul07;
+
+public class MyCar {
+public void applyBrake() {
+	// TODO Auto-generated method stub
+	System.out.println("apply brake");
+
+}
+private void applyGear() {
+	// TODO Auto-generated method stub
+	System.out.println("apply gear");
+
+}
+private void swithOnAc() {
+	// TODO Auto-generated method stub
+	System.out.println("swith on ac");
+
+}
+private void applyAccelerate() {
+	// TODO Auto-generated method stub
+	System.out.println("apply accelerate");
+
+}
+public static void main(String[] args) {
+	MyCar c=new MyCar();
+	c.applyBrake();
+	c.applyGear();
+	c.swithOnAc();
+	c.applyAccelerate();
+}
+}
