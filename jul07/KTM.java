6f0b21a744bffb42bd00f694234c15f9991f0a47
Abinesh19 <107668169+Abinesh19@users.noreply.github.com>
2022-07-20 22:51:22 +0530

    Add files via upload
    JUL07.DAY20

---
+++ jul07/KTM.java
@@ -0,0 +1,18 @@
+package day20.jul07;
+
+public class KTM {
+	public static void main(String[] args) {
+		int noOfWheels=2;
+		
+		short noOfMirrors=2;
+		boolean isPunctured=false;
+		
+		long chassisNumber=123456789987654321L;
+		String bikeName="ktm rc 200";
+		double mileage=38.00;
+		System.out.println(noOfWheels+":"+noOfMirrors+":"+isPunctured+":"+chassisNumber+":"+bikeName+":"+mileage);
+		
+				
+	}
+
+}
